d6cc4dfd24d4865482bda93deca4fa1f63ee5473
Reese <rdickinson@dataforgelabs.com>
2024-12-13 15:56:17 -0600

    more

--- modules/databricks_workspace/main.tf
+++ modules/databricks_workspace/main.tf
@@ -11,6 +11,9 @@ terraform {
   }
 }
 
+data "databricks_catalogs" "all" {}
+data "databricks_current_metastore" "this" {}
+
 locals {
   default_catalog = [
     for catalog in data.databricks_catalogs.all.ids : catalog
@@ -106,13 +109,8 @@ resource "databricks_mount" "datalake_mount" {
 
 }
 
-data "databricks_metastore" "existing_metastore" {
-  for_each = toset(var.enable_unity_catalog ? [var.region] : [])
-  region   = each.value
-}
-
 resource "databricks_metastore" "unity_catalog" {
-  count = var.enable_unity_catalog && length(data.databricks_metastore.existing_metastore) == 0  ? 1 : 0
+  count = var.enable_unity_catalog && data.databricks_current_metastore.this.metastore_id == ""  ? 1 : 0
   name          = "${var.environment_prefix}_unitycatalog"
   storage_root = format("abfss://%s@%s.dfs.core.windows.net/",
     azurerm_storage_data_lake_gen2_filesystem.datalake.name,
@@ -124,15 +122,15 @@ resource "databricks_metastore" "unity_catalog" {
 }
 
 resource "databricks_metastore_assignment" "existing_binding" {
-  count          = var.enable_unity_catalog && length(local.data.databricks_metastore.existing_metastore) > 0 ? 1 : 0
-  metastore_id   = data.databricks_metastore.existing_metastore[0].id 
+  count          = var.enable_unity_catalog && data.databricks_current_metastore.this.metastore_id == "" ? 1 : 0
+  metastore_id   = data.databricks_current_metastore.this.metastore_id 
   workspace_id   = azurerm_databricks_workspace.main.workspace_id
 
   depends_on = [ databricks_metastore.unity_catalog ]
 }
 
 resource "databricks_metastore_assignment" "workspace_binding" {
-  count        = var.enable_unity_catalog && length(local.data.databricks_metastore.existing_metastore) == 0 ? 1 : 0
+  count        = var.enable_unity_catalog && data.databricks_current_metastore.this.metastore_id != "" ? 1 : 0
   workspace_id = azurerm_databricks_workspace.main.workspace_id
   metastore_id = databricks_metastore.unity_catalog[0].id
 
